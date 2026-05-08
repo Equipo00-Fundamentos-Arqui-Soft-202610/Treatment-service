7302ec23d8a2d1959e65f2ebf869a436b56914ba
WoodsDos <javier.gonzales.2202@gmail.com>
2026-05-08 00:11:05 -0500

    chore: add EF Core and MySQL dependencies

--- MediTrack.TreatmentService.API/obj/Debug/net8.0/MediTrack.TreatmentService.API.AssemblyInfo.cs
+++ MediTrack.TreatmentService.API/obj/Debug/net8.0/MediTrack.TreatmentService.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MediTrack.TreatmentService.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e46d33a144bf9338045cbe3accb5829bea7bf411")]
 [assembly: System.Reflection.AssemblyProductAttribute("MediTrack.TreatmentService.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MediTrack.TreatmentService.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
